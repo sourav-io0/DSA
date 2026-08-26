74e9e191f79f21511dfffb538d7234371ed5783b
sourav-io0 <souravnextgen00@gmail.com>
2026-08-26 21:39:31 +0530

    replace elements with greatest

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def runningSum(self, nums: List[int]) -> List[int]:
-        total=0
-        for i in range(len(nums)):
-            total=total+nums[i]
-            nums[i]=total
-        return nums 
-
-        
